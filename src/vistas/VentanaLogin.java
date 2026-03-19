4aa6734dba26fcb92295a4375404e0735811aaaa
Patricia Aguado Marín <patriciaveggie@gmail.com>
2026-03-19 10:33:56 +0100

    cambio de driver por corrupción

--- src/vistas/VentanaLogin.java
+++ src/vistas/VentanaLogin.java
@@ -29,6 +29,7 @@ public class VentanaLogin extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("LOGIN");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
